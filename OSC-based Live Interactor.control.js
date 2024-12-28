d4174d7352b57c123dd4be208720b5257735d99f
Barnabás Oláh-Lautner <barna.olah@gmail.com>
2024-12-28 10:15:28 +0100

    Improved interactor

--- OSC-based Live Interactor.control.js
+++ OSC-based Live Interactor.control.js
@@ -3,14 +3,18 @@ loadAPI(19);
 host.setShouldFailOnDeprecatedUse(true);
 host.defineController("OSC", "OSC-based Live Interactor", "0.1", "7b0dec63-840c-4a22-b710-2936d4a0aed6", "briangrassfield");
 var trackCount = 0;
+var trackBank;
+var masterTrack;
+var actualTrackCount = 0;
+var trackBankLength;
 
 function init() {
 
-   var trackBank = host.createMainTrackBank(16, 0, 0); 
+   trackBank = host.createMainTrackBank(16, 0, 0); 
 
    var osc = host.getOscModule();
   
-   var targetHost = "192.168.64.36";
+   var targetHost = "127.0.0.1";
    var sendPort = 9100;
    var receivePort = 8100;
    
@@ -19,153 +23,142 @@ function init() {
 
    ledSwitch(target, "/led/online", true);
    
-   var length = trackBank.getSizeOfBank();
-   println("Track bank size: " + length);
-
-   var actualTrackCount = 0;
-   for (var i = 0; i < length; i++) {
-      println("Checking track " + i);
-      var track = trackBank.getItemAt(i);
-      track.exists().markInterested();
-      track.exists().addValueObserver(function(exists) {
-         if (exists) {
-            actualTrackCount++;
-         }
-      });
-   }
-
-   host.scheduleTask(function() {
-      println("Actual track count: " + actualTrackCount);
-
-      if(actualTrackCount < 16)
-      {
-         println("Track bank size is less than 16, exiting...");
-         return;
-      }
-      
-      var track1 = trackBank.getItemAt(0);
-      var track2 = trackBank.getItemAt(1);
-      var track3 = trackBank.getItemAt(2);
-      var track4 = trackBank.getItemAt(3);
-      var track5 = trackBank.getItemAt(4);
-      var track6 = trackBank.getItemAt(5);
-      var track7 = trackBank.getItemAt(6);
-      var track8 = trackBank.getItemAt(7);
-      var track9 = trackBank.getItemAt(8);
-      var track10 = trackBank.getItemAt(9);
-      var track11 = trackBank.getItemAt(10);
-      var track12 = trackBank.getItemAt(11);
-      var track13 = trackBank.getItemAt(12);
-      var track14 = trackBank.getItemAt(13);
-      var track15 = trackBank.getItemAt(14);
-      var track16 = trackBank.getItemAt(15);
-
-      var masterTrack = host.createMasterTrack(0);
-
-      var receiveSpace = osc.createAddressSpace();
-      receiveSpace.setShouldLogMessages(true);
-      receiveSpace.registerMethod("/controls/sync", ",f", "Sync function", function (source, message)
-      {
-         println("Received: " + message.getAddressPattern() + " " + message.getTypeTag() + " " + message.getFloat(0));
-         refreshInterface(target, trackBank);
-      });
-      receiveSpace.registerMethod("/controls/masterfilter", ",f", "Master Filter", function (source, message)
-      {
-         println("Received: " + message.getAddressPattern() + " " + message.getTypeTag() + " " + message.getFloat(0));
-         
-         
-      });
-
-      receiveSpace.registerDefaultMethod (function (source, message)
-      {
-         println("Received: " + message.getAddressPattern());
-      });
-      osc.createUdpServer(receivePort, receiveSpace);
-      
-      
-      track1.mute().addValueObserver(function (isMuted) {
-         ledSwitch(target, "/led/track1", !isMuted);
-      });
-
-      track2.mute().addValueObserver(function (isMuted) {
-         ledSwitch(target, "/led/track2", !isMuted);
-      });
-
-      track3.mute().addValueObserver(function (isMuted) {
-         ledSwitch(target, "/led/track3", !isMuted);
-      });
-
-      track4.mute().addValueObserver(function (isMuted) {
-         ledSwitch(target, "/led/track4", !isMuted);
-      });
-      
-      track5.mute().addValueObserver(function (isMuted) {
-         ledSwitch(target, "/led/track5", !isMuted);
-      });
-
-      track6.mute().addValueObserver(function (isMuted) {
-         ledSwitch(target, "/led/track6", !isMuted);
-      });
-
-      track7.mute().addValueObserver(function (isMuted) {
-         ledSwitch(target, "/led/track7", !isMuted);
-      });
-      
-      track8.mute().addValueObserver(function (isMuted) {
-         ledSwitch(target, "/led/track8", !isMuted);
-      });
-      
-      track9.mute().addValueObserver(function (isMuted) {
-         ledSwitch(target, "/led/track9", !isMuted);
-      });
-      
-      track10.mute().addValueObserver(function (isMuted) {
-         ledSwitch(target, "/led/track10", !isMuted);
-      });
-      
-      track11.mute().addValueObserver(function (isMuted) {
-         ledSwitch(target, "/led/track11", !isMuted);
-      });
-      
-      track12.mute().addValueObserver(function (isMuted) {
-         ledSwitch(target, "/led/track12", !isMuted);
-      });
-      
-      track13.mute().addValueObserver(function (isMuted) {
-         ledSwitch(target, "/led/track13", !isMuted);
-      });
-      
-      track14.mute().addValueObserver(function (isMuted) {
-         ledSwitch(target, "/led/track14", !isMuted);
-      });
-      
-      track15.mute().addValueObserver(function (isMuted) {
-         ledSwitch(target, "/led/track15", !isMuted);
-      });
-      
-      track16.mute().addValueObserver(function (isMuted) {
-         ledSwitch(target, "/led/track16", !isMuted);
-      });
-
-      ledSwitch(target, "/led/checked", true);
-
-      // TODO: Perform further initialization here.
-      println("OSC-based Live Interactor initialized!");
-   }, 1000);
-
-   // Add track observer to restart script when a new track is added
-   trackBank.addTrackCountObserver(function(count) {
-      if (count > trackCount) {
-         trackCount = count;
-         println("Track count changed, restarting script...");
-         init();
-      }
+   trackBank.itemCount().markInterested();
+   trackBank.itemCount().addValueObserver(function(count) {
+      trackBankLength = count;
+      println("Track bank size: " + trackBankLength);
    });
+   
+   masterTrack = host.createMasterTrack(0);
+   
+   // for (var i = 0; i < trackBankLength; i++) {
+   //    println("Checking track " + i);
+   //    var track = trackBank.getItemAt(i);
+   //    track.exists().markInterested();
+   //    track.exists().addValueObserver(function(exists) {
+   //       if (exists) {
+   //          actualTrackCount++;
+   //       }
+   //    });
+   // }
+
+   println("Creating address space");
+   var receiveSpace = osc.createAddressSpace();
+   receiveSpace.setShouldLogMessages(true);
+
+   println("Registering sync receiver method");
+   receiveSpace.registerMethod("/controls/sync", ",f", "Sync function", function (source, message)
+   {
+      println("Received: " + message.getAddressPattern() + " " + message.getTypeTag() + " " + message.getFloat(0));
+      refreshInterface(target, trackBank);
+   });
+
+   println("Registering masterfilter receiver method");
+   receiveSpace.registerMethod("/controls/masterfilter", ",f", "Master Filter", function (source, message)
+   {
+      println("Received: " + message.getAddressPattern() + " " + message.getTypeTag() + " " + message.getFloat(0));         
+   });
+
+   receiveSpace.registerDefaultMethod (function (source, message)
+   {
+      println("Received: " + message.getAddressPattern());
+   });
+
+   println("Create OSC server");
+   osc.createUdpServer(receivePort, receiveSpace);
+
+   var track1 = trackBank.getItemAt(0);
+   var track2 = trackBank.getItemAt(1);
+   var track3 = trackBank.getItemAt(2);
+   var track4 = trackBank.getItemAt(3);
+   var track5 = trackBank.getItemAt(4);
+   var track6 = trackBank.getItemAt(5);
+   var track7 = trackBank.getItemAt(6);
+   var track8 = trackBank.getItemAt(7);
+   var track9 = trackBank.getItemAt(8);
+   var track10 = trackBank.getItemAt(9);
+   var track11 = trackBank.getItemAt(10);
+   var track12 = trackBank.getItemAt(11);
+   var track13 = trackBank.getItemAt(12);
+   var track14 = trackBank.getItemAt(13);
+   var track15 = trackBank.getItemAt(14);
+   var track16 = trackBank.getItemAt(15);
+
+   track1.mute().addValueObserver(function (isMuted) {
+      ledSwitch(target, "/led/track1", !isMuted);
+   });
+
+   track2.mute().addValueObserver(function (isMuted) {
+      ledSwitch(target, "/led/track2", !isMuted);
+   });
+
+   track3.mute().addValueObserver(function (isMuted) {
+      ledSwitch(target, "/led/track3", !isMuted);
+   });
+
+   track4.mute().addValueObserver(function (isMuted) {
+      ledSwitch(target, "/led/track4", !isMuted);
+   });
+   
+   track5.mute().addValueObserver(function (isMuted) {
+      ledSwitch(target, "/led/track5", !isMuted);
+   });
+
+   track6.mute().addValueObserver(function (isMuted) {
+      ledSwitch(target, "/led/track6", !isMuted);
+   });
+
+   track7.mute().addValueObserver(function (isMuted) {
+      ledSwitch(target, "/led/track7", !isMuted);
+   });
+   
+   track8.mute().addValueObserver(function (isMuted) {
+      ledSwitch(target, "/led/track8", !isMuted);
+   });
+   
+   track9.mute().addValueObserver(function (isMuted) {
+      ledSwitch(target, "/led/track9", !isMuted);
+   });
+   
+   track10.mute().addValueObserver(function (isMuted) {
+      ledSwitch(target, "/led/track10", !isMuted);
+   });
+   
+   track11.mute().addValueObserver(function (isMuted) {
+      ledSwitch(target, "/led/track11", !isMuted);
+   });
+   
+   track12.mute().addValueObserver(function (isMuted) {
+      ledSwitch(target, "/led/track12", !isMuted);
+   });
+   
+   track13.mute().addValueObserver(function (isMuted) {
+      ledSwitch(target, "/led/track13", !isMuted);
+   });
+   
+   track14.mute().addValueObserver(function (isMuted) {
+      ledSwitch(target, "/led/track14", !isMuted);
+   });
+   
+   track15.mute().addValueObserver(function (isMuted) {
+      ledSwitch(target, "/led/track15", !isMuted);
+   });
+   
+   track16.mute().addValueObserver(function (isMuted) {
+      ledSwitch(target, "/led/track16", !isMuted);
+   });
+
+
+   refreshInterface(target);
 
 }
 
-function refreshInterface(target, trackBank)
+function refreshInterface(target)
 {
+   ledSwitch(target, "/led/online", true);
+
+   
+
    var track1 = trackBank.getItemAt(0);
    var track2 = trackBank.getItemAt(1);
    var track3 = trackBank.getItemAt(2);
@@ -200,6 +193,23 @@ function refreshInterface(target, trackBank)
    ledSwitch(target, "/led/track15", !track15.mute().get());
    ledSwitch(target, "/led/track16", !track16.mute().get());
 
+   host.scheduleTask(function() {
+      //println("Actual track count: " + actualTrackCount);
+      println("Actual track count: " + trackBankLength);
+
+      if(trackBankLength < 16)
+      {
+         println("Track bank size is less than 16, exiting...");
+         ledSwitch(target, "/led/checked", false);
+         return;
+      }
+      
+
+      ledSwitch(target, "/led/checked", true);
+
+      // TODO: Perform further initialization here.
+      println("OSC-based Live Interactor initialized!");
+   }, 20);
 }
 
 function ledSwitch(target, address, status)
